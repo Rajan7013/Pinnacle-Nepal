1645e0ebe1efd5ead60aab0ceba14451aab2da15
Rajan Prasaila Yadav <rajanprasaila@gmail.com>
2025-12-25 05:18:20 +0530

    Updates

--- _next/static/7hFhB0RxGe2uklPQVFk7a/_buildManifest.js
+++ _next/static/7hFhB0RxGe2uklPQVFk7a/_buildManifest.js
@@ -3,8 +3,8 @@ self.__BUILD_MANIFEST = {
     "afterFiles": [],
     "beforeFiles": [
       {
-        "source": "/Pinnacle-Nepal//_next/:path+",
-        "destination": "/Pinnacle-Nepal/_next/:path+"
+        "source": "/pinnacle-nepal//_next/:path+",
+        "destination": "/pinnacle-nepal/_next/:path+"
       }
     ],
     "fallback": []
